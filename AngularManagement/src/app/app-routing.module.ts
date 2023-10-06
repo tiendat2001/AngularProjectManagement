8be5fd34ecaa441a01b4ddf3a819c88ca95aa92e
dat <clonetiendat@gmail.com>
2023-10-06 16:23:37 +0700

    Add some component

--- AngularManagement/src/app/app-routing.module.ts
+++ AngularManagement/src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { EditStudentComponent } from './components/edit-student/edit-student.component';
+import { ListStudentComponent } from './components/list-student/list-student.component';
+import { LoginStudentComponent } from './components/login-student/login-student.component';
+import { RegistrationStudentComponent } from './components/registration-student/registration-student.component';
 
 const routes: Routes = [];
 
